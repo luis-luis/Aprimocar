a91cfd2d358be9417100e910cb0d8fba16a681b2
luis-luis <luisdebortoli7@gmail.com>
2025-01-28 14:12:06 -0300

    Alterações visuais iniciais

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <title>Aprimocar Garage & Detailing</title>
+
+  <!-- Custom styles for this template -->
+  <link href="carousel.css" rel="stylesheet">
+
+  <link href="css/bootstrap.css" rel="stylesheet">
+
+  <!-- Favicons
+  <link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+  <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+  <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+  <link rel="manifest" href="/docs/5.3/assets/img/favicons/manifest.json">
+  <link rel="mask-icon" href="/docs/5.3/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
+  <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon.ico">
+  <meta name="theme-color" content="#712cf9"> -->
+
+</head>
+
+<body>
+
+  @yield('content')
+
+  <!-- FOOTER -->
+  <footer class="container">
+    <p class="float-end"><a href="#">Back to top</a></p>
+    <p>&copy; 2025 Aprimocar Garage. &middot;
+  </footer>
+  </main>
+  <script src="{{ asset('js/popper.js')}}"></script>
+  <script src="{{ asset('bootstrap.bundle.min.js')}}"></script>
+  <script src="{{ asset('js/bootstrap.js')}}" type="module"></script>
+  <script src="{{ asset('js/color-modes.js')}}"></script>
+
+</body>
+
+</html>

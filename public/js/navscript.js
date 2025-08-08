cb5b6c80f0c822b634d8d664a1c95a61aacbb83b
luis-luis <luisdebortoli7@gmail.com>
2025-08-08 10:12:30 -0300

    atualizado navbar

--- public/js/navscript.js
+++ public/js/navscript.js
@@ -1,20 +1,15 @@
-// navscript.js
-
-const nav = document.getElementById('navmenu');
+const header = document.getElementById('header');
 let lastScrollTop = 0;
 
 window.addEventListener('scroll', () => {
   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if(scrollTop === 0) {
-    // No topo, mostra a navbar
-    nav.classList.remove('hidden');
+    header.classList.remove('hidden');
   } else if(scrollTop > lastScrollTop) {
-    // Rolando para baixo, esconde a navbar
-    nav.classList.add('hidden');
+    header.classList.add('hidden');
   } else {
-    // Rolando para cima, mostra a navbar
-    nav.classList.add('hidden');
+    header.classList.remove('hidden');
   }
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Evita scroll negativo
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
